a3d6d4e0bda533af49d9b38820d2b00c01795206
Amit-10apr <amit10apr92@gmail.com>
2022-05-17 21:29:11 +0530

    create utility servicve to fetch occupation list n bind to dropdowne

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,23 @@ export class AppComponent implements OnInit {
 
   premiumCalForm!: FormGroup;
   submitted = false;
+  occupationList : any[] = [];
+  City: string[] = ['Florida', 'South Dakota', 'Tennessee', 'Michigan'];
 
   constructor(
     private fb: FormBuilder,
     private utilityService: UtilityService
-  ) { }
+  ) { 
+    this.occupationList = this.utilityService.getOccupationWithFactor();
+    console.log("occupation is " +this.occupationList[1].occupation);
+  }
 
   ngOnInit() {
     this.premiumCalForm = this.fb.group({
       name: ['', Validators.required],
       dob: ['', [Validators.required]],
-      occupation: ['', Validators.required],
-      suminsured: ['', Validators.required],
+      occupations: ['', Validators.required],
+      suminsured: ['', Validators.required]
     }
     );
   }
@@ -31,15 +36,27 @@ export class AppComponent implements OnInit {
   get premiumFormControl() {
     return this.premiumCalForm.controls;
   }
+  get OccupationName() {
+    return this.premiumCalForm.get('occupations');
+  }
 
+  changeOccupation(e: any) {
+    this.OccupationName?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+    console.log(this.premiumCalForm.get('occupations')?.value)
+    var age = this.utilityService.calculateAge(this.premiumCalForm.get('dob')?.value);
+  }
   onSubmit() {
     this.submitted = true;
-    if (this.premiumCalForm.valid) {
-      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
-      console.table(this.premiumCalForm.value);
-    }
+    // if (this.premiumCalForm.valid) {
+    //   alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+    //   console.table(this.premiumCalForm.value);
+    // }
 
     var age = this.utilityService.calculateAge(this.premiumCalForm.get('dob')?.value)
+   // var occupation = this.utilityService.getOccupationWithFactor();
+    console.log("occupation is " +this.occupationList[1].factor);
     console.log("age is " + age);
     console.log(this.premiumCalForm.get('name')?.value)
     console.log(this.premiumCalForm.get('dob')?.value)
--- src/app/utility.service.ts
+++ src/app/utility.service.ts
@@ -5,11 +5,50 @@ import { Injectable } from '@angular/core';
 })
 export class UtilityService {
 
+  occupation_rating = [
+
+    { Occupation : 'Cleaner', Rating: 'Light Manual'},
+    { Occupation : 'Doctor', Rating: 'Professional'},
+    { Occupation : 'Author', Rating: 'White Collar'},
+    { Occupation : 'Farmer', Rating: 'Heavy Manual'},
+    { Occupation : 'Mechanic', Rating: 'Heavy Manual'},
+    { Occupation : 'Florist', Rating: 'Light Manual'}
+
+  ]
+
+  rating_factor = [
+
+    { Rating : 'Professional', Factor: 1.0 },
+    { Rating : 'White Collar', Factor: 1.25 },
+    { Rating : 'Light Manual', Factor: 1.50 },
+    { Rating : 'Heavy Manual', Factor: 1.75 }
+    
+  ]
+
   constructor() { }
 
    calculateAge(birthday:Date) { 
     var ageDifMs = Date.now() - new Date(birthday).getTime();
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
     return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
+  }
+
+  getOccupationWithFactor() {
+
+    var results = [];
+    for (var i=0; i<this.occupation_rating.length; i++) {
+        for (var j=0; j<this.rating_factor.length; j++) {
+            if (this.occupation_rating[i].Rating === this.rating_factor[j].Rating) {
+                results.push({
+                    occupation: this.occupation_rating[i].Occupation, 
+                    rating: this.occupation_rating[i].Rating,
+                    factor: this.rating_factor[j].Factor
+                });
+            }
+        }
+    }
+    console.log(results)
+    return results;
+    
+  }
 }
