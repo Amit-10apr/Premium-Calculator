1b139fcf26540f62801d6e146bbae2f997b44f0e
Amit-10apr <amit10apr92@gmail.com>
2022-05-17 11:02:01 +0530

    Created initial UI for premium calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'premium-calculator';
+
+  premiumCalForm!: FormGroup;
+  submitted = false;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.premiumCalForm = this.fb.group({
+      name: ['', Validators.required],
+      dob: ['', [Validators.required]],
+      occupation: ['', Validators.required],
+      suminsured: ['', Validators.required],
+    }
+    );
+  }
+
+  get premiumFormControl() {
+    return this.premiumCalForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.premiumCalForm.valid) {
+      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+      console.table(this.premiumCalForm.value);
+    }
+  }
 }
